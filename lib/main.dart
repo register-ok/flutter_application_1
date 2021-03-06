8d52ffe7a18530426e09326eb94e0f3cb0781e0d
register-ok <ok0897575431@gmail.com>
2021-03-06 14:32:24 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        drawer: Drawer(
-          child: Text("Hello Drawer"),
-        ),
-        appBar: AppBar(
-          title: Text(
-            "My First Project",
-          ),
-        ),
-        body: Center(
-          child: Text("Hello world"),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "home"
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.phone),
-              label: "Phone"
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.dashboard_customize),
-              label: "dashboard_customize"
-            ),
-          ],
-        ),
-      ),
-    ),
-  );
+void main(){
+  runApp()
 }
